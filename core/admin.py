dfe394e56df01c8bf28a392aa3d19a499449def9
Anneksant <annekaroline20133@gmail.com>
2025-12-16 18:39:37 -0300

    Register ProjetoAdmin for Projeto model
    Added custom admin configuration for Projeto model.

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Projeto, Equipe
 admin.site.register(Projeto)
 admin.site.register(Equipe)
+
+@admin.register(Projeto)
+class ProjetoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'cliente', 'status', 'data_inicio')
+    list_filter = ('status',)
+    search_fields = ('titulo', 'cliente')
